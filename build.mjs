42690690e92884c2dce041aaa0b78ded51a49aac
Ryan <gyanreyer@gmail.com>
2022-10-29 12:15:59 -0400

    Add non-esm build output options

--- build.mjs
+++ build.mjs
@@ -1,17 +1,49 @@
 import esbuild from "esbuild";
 import { minifyTemplates, writeFiles } from "esbuild-minify-templates";
+import fs from "fs";
+
+const outputDir = "dist";
+
+// Clean up any files in the output directory, or make sure the output directory exists
+if (fs.existsSync(outputDir)) {
+  fs.readdirSync(outputDir).forEach((file) => {
+    fs.unlinkSync(`${outputDir}/${file}`);
+  });
+} else {
+  fs.mkdirSync(outputDir);
+}
 
 const shouldWatch = process.argv.includes("--watch");
 
-await esbuild.build({
+const sharedOptions = {
   entryPoints: ["src/hover-video-player.ts"],
-  outfile: "dist/index.js",
   plugins: [minifyTemplates(), writeFiles()],
   write: false, // this needs to be left to the writeFiles plugin
   minify: true,
+  sourcemap: true,
   // Mangle all internal private properties, which start with an underscore
   mangleProps: /^_.+$/,
-  sourcemap: true,
   watch: shouldWatch,
   logLevel: "info",
-});
+};
+
+await Promise.all([
+  // esm build
+  esbuild.build({
+    ...sharedOptions,
+    outfile: `${outputDir}/index.mjs`,
+    format: "esm",
+  }),
+  // cjs build
+  esbuild.build({
+    ...sharedOptions,
+    outfile: `${outputDir}/index.cjs`,
+    format: "cjs",
+  }),
+  // iife build for browsers
+  esbuild.build({
+    ...sharedOptions,
+    outfile: `${outputDir}/index.js`,
+    format: "iife",
+  }),
+]);
